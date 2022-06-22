72daf2bbdb6e9ab0565eddd8bbdde0a9a82b1cc8
Daniel Schwen <daniel.schwen@inl.gov>
2022-06-22 11:02:49 -0600

    add hit parser tests

--- server/src/hit_parser.ts
+++ server/src/hit_parser.ts
@@ -6,14 +6,14 @@
 
 import * as Parser from 'web-tree-sitter';
 import * as path from 'path';
+import { Position } from 'vscode-languageserver/node';
 
 export interface HITBlock {
-    path: string;
+    path: string[];
     node: Parser.SyntaxNode;
 }
 
-export interface HITParameterList
-{
+export interface HITParameterList {
     [key: string]: string;
 }
 
@@ -40,7 +40,7 @@ export class HITParser {
         }
     }
 
-    onReady(f : Function) {
+    onReady(f: Function) {
         if (this.parser) {
             // if the parser is already initialized execute f right away
             f();
@@ -58,37 +58,57 @@ export class HITParser {
         }
     }
 
+    // Removes quotes if possible (i.e. the string contains no whitespace or other quotes)
+    static normalizeValue(val : string) : string {
+        const okSingleQuote = /^'.*["\s].*'$/;
+        const okDoubleQuote = /^".*['\s].*"$/;
+        if (okSingleQuote.test(val) || okDoubleQuote.test(val)) {
+            return val;
+        }
+        return val;
+    }
+
     getBlockList(): HITBlock[] {
         var blockList: HITBlock[] = [];
-        function buildBlockList(node: Parser.SyntaxNode, oldPath?: string) {
-            var block, c, i, len, newPath: string, ref;
+        function buildBlockList(node: Parser.SyntaxNode, oldPath: string[]) {
+            var block, c, i, len, ref;
             ref = node.children;
             for (i = 0, len = ref.length; i < len; i++) {
                 c = ref[i];
                 if (c.type === 'top_block' || c.type === 'block') {
-                    block = c.children[1].text;
-                    if (block.slice(0, 2) === './') {
-                        block = block.slice(2);
+                    block = c.children[1].text.split('/');
+                    if (block[0] == '.') {
+                        block = block.slice(1);
                     }
-                    newPath = (oldPath ? oldPath + '/' : '') + block;
+
+                    var newPath = [...oldPath, ...block];
                     blockList.push({ path: newPath, node: c });
                     buildBlockList(c, newPath);
                 }
             }
         };
         if (this.tree) {
-            buildBlockList(this.tree.rootNode);
+            buildBlockList(this.tree.rootNode, []);
         }
         return blockList;
     }
 
-    findBlock(path : string) : Parser.SyntaxNode | null {
+    // Get the HIT syntax node for a block specified by a path. The path may either be
+    // a string with path components delimited by '/' or an array of path components.
+    findBlock(pathParam: string | string[]): Parser.SyntaxNode | null {
         if (!this.tree) {
             return null;
         }
 
-        function traverseBlocks(node: Parser.SyntaxNode, oldPath?: string) : Parser.SyntaxNode | null {
-            var block : string, c : Parser.SyntaxNode, i, len,  ref : Parser.SyntaxNode[];
+        var path: string;
+        if (typeof pathParam === 'string') {
+            path = pathParam;
+        } else {
+            path = pathParam.join('/');
+        }
+
+        function traverseBlocks(node: Parser.SyntaxNode, oldPath?: string): Parser.SyntaxNode | null {
+            var block: string, c: Parser.SyntaxNode, i, len, ref: Parser.SyntaxNode[];
             ref = node.children;
             for (i = 0, len = ref.length; i < len; i++) {
                 c = ref[i];
@@ -128,12 +148,82 @@ export class HITParser {
         return params;
     }
 
-    getPathParameters(path : string): HITParameterList {
+    getBlockParameter(node: Parser.SyntaxNode, name: string) : string | null {
+        for (var i = 0, len = node.children.length; i < len; i++) {
+            var c = node.children[i];
+            if (c.type === 'parameter_definition' && c.children[0].text == name) {
+                return c.children[2].text;
+            }
+        }
+        return null;
+    }
+
+    getPathParameters(path: string | string[]): HITParameterList {
         var node = this.findBlock(path);
         if (!node) {
             return {};
         }
         return this.getBlockParameters(node);
+    }
+
+    getBlockAtPosition(p: Position): HITBlock | null {
+        function recurseCurrentConfigPath(block: HITBlock): HITBlock {
+            var newBlock: HITBlock;
+            var ref = block.node.children;
+            for (var i = 0, len = ref.length; i < len; i++) {
+                var c = ref[i];
+                if (c.type !== 'top_block' && c.type !== 'block' && c.type !== 'ERROR') {
+                    continue;
+                }
+
+                // check if we are inside a block or top_block
+                var cs = c.startPosition;
+                var ce = c.endPosition;
+
+                // outside row range
+                if (p.line < cs.row || p.line > ce.row) {
+                    continue;
+                }
+                // in starting row but before starting column
+                if (p.line === cs.row && p.character < cs.column) {
+                    continue;
+                }
+                // in ending row but after ending column
+                if (p.line === ce.row && p.character > ce.column) {
+                    continue;
+                }
+                // if the block does not contain a valid path subnode we give up
+                if (c.children.length < 2 || c.children[1].type !== 'block_path') {
+                    return block;
+                }
+                // first block_path node
+                if (c.type !== 'ERROR') {
+                    if (c.children[1].startPosition.row >= p.line) {
+                        continue;
+                    }
+                    newBlock = { node: c, path: [...block.path, ...c.children[1].text.replace(/^\.\//, '').split('/')] };
+                } else {
+                    var ref1 = c.children, path = block.path;
+                    // if we are in an ERROR block (unclosed) we should try to pick more path elements
+                    for (var j = 0, len1 = ref1.length; j < len1; j++) {
+                        var c2 = ref1[j];
+                        if (c2.type !== 'block_path' || c2.startPosition.row >= p.line) {
+                            continue;
+                        }
+                        path = [...path, ...c2.text.replace(/^\.\//, '').split('/')];
+                    }
+                    newBlock = { node: c, path: path };
+                }
+                return recurseCurrentConfigPath(newBlock);
+            }
+            return block;
+        };
+
+        if (this.tree) {
+            return recurseCurrentConfigPath({ path: [], node: this.tree.rootNode });
+        } else {
+            return null;
+        }
     }
 
     // // determine the active input file path at the current position
--- server/test/runTest.js
+++ server/test/runTest.js
@@ -10,39 +10,98 @@ var hp = require('../out/hit_parser.js');
 
 var p = new hp.HITParser();
 
-function testGetBlockList() {
-    // get the list of all blocks
-    var bl = p.getBlockList();
+function testGetBlockList(gold) {
+  // get the list of all blocks
+  var bl = p.getBlockList();
 
-    // extract just the block paths
-    var paths = bl.map(b => b.path);
-    paths.sort();
+  // extract just the block paths
+  var paths = bl.map(b => b.path);
+  paths.sort();
 
-    assert.deepEqual(paths, ["BCs", "BCs/left", "BCs/right", "Executioner", "Kernels", "Kernels/diff", "Mesh", "Outputs", "Variables", "Variables/u"]);
+  assert.deepEqual(paths, gold);
 }
 
 function testFindBlock() {
-    // get the hit node for a specified block
-    var n = p.findBlock('BCs/right');
-    assert.equal(n.children[1].text, 'right');
+  // get the hit node for a specified block
+  var n = p.findBlock('Executioner');
+  assert.equal(p.getBlockParameter(n, 'solve_type'), "'PJFNK'");
+}
+
+function testGetBlockParameter() {
+  var n = p.findBlock('BCs/right');
+  var gold = { type: 'DirichletBC', variable: 'u', boundary: 'left', value: '0' };
+
+  assert.deepEqual(p.getPathParameters('BCs/left'), gold);
+  assert.deepEqual(p.getPathParameters(['BCs', 'left']), gold);
 }
 
 function testGetPathParameters() {
-    assert.deepEqual(p.getPathParameters('BCs/left'), { type: 'DirichletBC', variable: 'u', boundary: 'left', value: '0' });
+  assert.deepEqual(p.getPathParameters('BCs/left'), { type: 'DirichletBC', variable: 'u', boundary: 'left', value: '0' });
 }
 
 p.onReady(() => {
-    // load a simple MOOSE input file
-    var t = fs.readFileSync('simple_diffusion.i', 'utf8');
+  //
+  // load a simple MOOSE input file
+  var t = fs.readFileSync('correct.i', 'utf8');
+  // and parse it
+  p.parse(t);
 
-    // and parse it
-    p.parse(t);
+  // run tests
+  testGetBlockList([['BCs'], ['BCs', 'left'], ['BCs', 'right'], ['Executioner'], ['Kernels'], ['Kernels', 'diff'], ['Mesh'], ['Outputs'], ['Variables'], ['Variables', 'u']]);
+  testFindBlock();
+  testGetBlockParameter();
+  testGetPathParameters();
 
-    // run tests
-    testGetBlockList();
-    testFindBlock();
-    testGetPathParameters();
+  //
+  // load a more complicated MOOSE input file
+  t = fs.readFileSync('multilevel.i', 'utf8');
+  // and parse it
+  p.parse(t);
 
-    // done
-    console.log('All passed.')
+  // run tests
+  assert.deepEqual(p.getBlockAtPosition({ line: 44, column: 22 }).path, ['Modules', 'TensorMechanics', 'Master', 'block2']);
+  testGetBlockList([
+    ['AuxKernels'],
+    ['AuxKernels', 'strain_theta'],
+    ['AuxKernels', 'stress_theta'],
+    ['AuxVariables'],
+    ['AuxVariables', 'strain_theta'],
+    ['AuxVariables', 'stress_theta'],
+    ['BCs'],
+    ['BCs', 'bottom'],
+    ['BCs', 'left'],
+    ['BCs', 'right'],
+    ['BCs', 'top'],
+    ['Debug'],
+    ['Executioner'],
+    ['GlobalParams'],
+    ['Materials'],
+    ['Materials', '_elastic_stress1'],
+    ['Materials', '_elastic_stress2'],
+    ['Materials', 'elasticity_tensor'],
+    ['Mesh'],
+    ['Mesh', 'block1'],
+    ['Mesh', 'block2'],
+    ['Mesh', 'generated_mesh'],
+    ['Modules', 'TensorMechanics', 'Master'],
+    ['Modules', 'TensorMechanics', 'Master', 'block1'],
+    ['Modules', 'TensorMechanics', 'Master', 'block2'],
+    ['Outputs']
+  ]);
+
+  //
+  // load an incomplete MOOSE input file
+  t = fs.readFileSync('incomplete.i', 'utf8');
+  // and parse it
+  p.parse(t);
+
+  // only complete blocks are returned for now
+  testGetBlockList([['Kernels'], ['Kernels', 'diff'], ['Mesh'], ['Variables'], ['Variables', 'u']]);
+  // getting an incomplete block (this needs to work to make autocompletion useful, as any new block
+  // the user is adding will likely be unclosed)
+  assert.deepEqual(p.getBlockAtPosition({ line: 22, column: 12 }).path, ['BCs', 'left']);
+
+
+  // done
+  console.log('All passed.')
 });
